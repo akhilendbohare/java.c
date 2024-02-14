f38d089ad4d27fc08d16ba9991892116f18fbabe
akhilendbohare <146517273+akhilendbohare@users.noreply.github.com>
2024-02-14 11:58:26 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class demo{
+public static void main(String args[]){
+int a,b;
+Scanner input=new Scanner(System.in);
+a=input.nextInt();
+b=input.nextInt();
+System.out.println("sum is"+(a+b));
+}
+}
