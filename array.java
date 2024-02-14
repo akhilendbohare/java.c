c0d2fc350c87684b89aa09d3d5db491f5a6f624b
akhilendbohare <146517273+akhilendbohare@users.noreply.github.com>
2024-02-14 11:56:43 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class arr{
+public static void main(String args[]){
+int a[]=new int[10];
+Scanner inp=new Scanner(System.in);
+for(int i=0;i<4;i++)
+{
+a[i]=inp.nextInt();
+}
+System.out.println("array is ");
+for(int i=0;i<4;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
